eec82e35e4f565f94f270326087e23ff50770bbd
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2026-04-29 21:25:58 +0200

    test installation flask

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hada test")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Le site de billetterie arrive !"
+
+if __name__ == '__main__':
+    app.run(debug=True)
