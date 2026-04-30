e9e4e23a380df6024805d1b697854b8e0995b5ff
sara <edirisara762@gmail.com>
2026-04-30 11:58:39 +0200

    base de donnees

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+# Connexion à PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:141039@localhost/tickets_spectacle'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'cle_secrete_a_changer'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def hello():
