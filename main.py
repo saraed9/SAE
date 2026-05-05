4cc1983d7d890ed1d745c4eb7491ebb94026a5be
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2026-05-05 18:48:43 +0200

    màj imports

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import datetime
+
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
