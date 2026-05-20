dc0ab62ca02d7324665e7af0e0f6761dabf0b8e3
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2026-05-20 02:44:02 +0200

    verif paiement carte bancaire

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ def login():
             return redirect('/')
         
         flash("Email ou mot de passe incorrect", "error")        
-        return redirect('/login')
+        return render_template('login.html', form_data=request.form)  # Renvoyer les données du formulaire pour éviter de les perdre
     
-    return render_template('login.html')
+    return render_template('login.html', form_data=None)
 
 @app.route('/forgot_password')
 def forgot_password():
@@ -94,9 +94,9 @@ def register():
             flash("Cet email est déjà utilisé", "info")
         elif result["error"] == "mdp_faible":
             flash("Le mot de passe est trop faible", "error")
-        return redirect('/register')
+        return render_template('registration.html', form_data=request.form)  # Renvoyer les données du formulaire pour éviter de les perdre
 
-    return render_template('registration.html')
+    return render_template('registration.html', form_data=None)
 
 @app.route('/logout')
 def logout():
@@ -208,6 +208,11 @@ def paiement():
         return redirect('/panier')
     
     if request.method == 'POST':
+        carteOk = verifCarte(request.form)  # Vérification des données de paiement (simulée)
+        if not carteOk:
+            # Si la validation échoue, on retourne à la page de paiement avec les données du formulaire pour éviter de les perdre
+            return render_template('paiement.html', form_data=request.form)
+
         total = 0
         quantite_totale = 0
         for item in panier:
@@ -242,7 +247,7 @@ def paiement():
             flash("Une erreur est survenue lors du paiement", "error")
             return redirect('/panier')
 
-    return render_template('paiement.html')
+    return render_template('paiement.html', form_data=None)
 
 @app.route('/confirmation')
 def confirmation():
@@ -255,7 +260,7 @@ def confirmation():
         flash("Aucune commande payée récemment", "info")
         return redirect('/')
 
-    order = Commande.query.filter_by(user_id=session['user_id']).order_by(Commande.id.desc()).first()
+    order = Commande.query.filter_by(id=commande_id, user_id=session['user_id']).first()
     if not order:
         flash("Aucune commande trouvée", "error")
         return redirect('/')
@@ -275,7 +280,78 @@ def confirmation():
             })
             
     return render_template('confirmation.html', order=order, details=details)
+
+
+def verifCarte(form):
+    card_name = form.get('card_name').strip()
+    card_number = form.get('card_number').replace(" ", "")
+    expiry = form.get('card_expiry').strip()
+    cvv = form.get('card_cvv').strip()
+
+    # Vérification du nom sur la carte
+    if not card_name or len(card_name) < 2:
+        flash("Le nom sur la carte n'est pas valie. (minimum 2 caractères)", "error")
+        return False
+
+    # Vérification du numéro de carte 
+    if not card_number.isdigit() or len(card_number) != 16:
+        flash("Numéro de carte invalide. Il doit contenir exactement 16 chiffres.", "error")
+        return False
     
+    if not verifNumCarte(card_number):
+        flash("Numéro de carte invalide. Le numéro ne respecte pas l'algorithme de Luhn.", "error")
+        return False
+
+    # Vérification de la date d'expiration
+    if not verifDateExpiration(expiry):
+        return False
+    
+    # Vérification du code CVV
+    if not cvv.isdigit() or len(cvv) != 3:
+        flash("Code CVV invalide. Il doit contenir exactement 3 chiffres.", "error")
+        return False
+
+    return True
+
+def verifNumCarte(numero_carte):
+    """Vérifie si un numéro de carte est mathématiquement valide (Algorithme de Luhn)."""
+    somme = 0
+    alterne = False
+    
+    # On parcourt les chiffres en partant de la fin
+    for chiffre in reversed(numero_carte):
+        num = int(chiffre)
+        if alterne:
+            num *= 2
+            if num > 9:
+                num -= 9
+        somme += num
+        alterne = not alterne
+        
+    return (somme % 10 == 0)
+
+def verifDateExpiration(expiry):
+    if not expiry or '/' not in expiry:
+        flash("Format de date d'expiration invalide (attendu: MM/AA).", "error")
+        return False
+    
+    parts = expiry.split('/')
+    month = int(parts[0])
+    year = int(parts[1]) + 2000 # On transforme '26' en '2026' pour comparer les dates
+
+    # Validation du mois et vérification que la carte n'est pas expirée
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+
+    if month < 1 or month > 12:
+        flash("Date d'expiration invalide (MM/AA).", "error")
+        return False
+    
+    if year < current_year or (year == current_year and month < current_month):
+        flash("La carte bancaire est expirée.", "error")
+        return False
+
+    return True
 
 if __name__ == '__main__':
     app.run(debug=False)
