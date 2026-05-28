d1c0406f6c248f29abceafd0defceb51ec0a997f
Mahereiti <Mahereiti.Teraiamano@uphf.fr>
2026-05-28 10:56:34 +0200

    lil changess

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ limiter = Limiter(
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Postgres113@localhost/Spectra'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')  # Utiliser une variable d'environnement pour la clé secrète en production
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fcd4ca06371442fad324310cf5032827b850643fc9def1af34c7b83341948d0f')  # Utiliser une variable d'environnement pour la clé secrète en production
 if not app.config['SECRET_KEY']:
     raise RuntimeError("SECRET_KEY non definie.")
 
@@ -289,4 +289,4 @@ def confirmation():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
